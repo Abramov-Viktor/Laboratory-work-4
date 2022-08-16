f800dcceb598ee885e783d4cba50ed0b5f3f4c2f
Abramov-Viktor <99405399+Abramov-Viktor@users.noreply.github.com>
2022-08-16 17:45:36 +0700

    Создан новый класс и тест для его инициализации

---
+++ UnitTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Millioner;
+
+namespace MillionerTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        
+        [TestMethod]
+        public void QuestionInitializeTest()
+        {
+            Question question = new Question();
+            Assert.IsNotNull(question);
+        }
+    }
+}
